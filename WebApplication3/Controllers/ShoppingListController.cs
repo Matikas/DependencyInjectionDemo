f448351e42b03872340e234abb0e79de8b38fdbf
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-01-26 20:00:21 +0200

    logging

--- WebApplication3/Controllers/ShoppingListController.cs
+++ WebApplication3/Controllers/ShoppingListController.cs
@@ -9,10 +9,12 @@ namespace WebApplication3.Controllers
     public class ShoppingListController : ControllerBase
     {
         private readonly IShoppingListRepository _shoppingList;
+        private readonly ILogger<ShoppingListController> _logger;
 
-        public ShoppingListController(IShoppingListRepository shoppingList)
+        public ShoppingListController(IShoppingListRepository shoppingList, ILogger<ShoppingListController> logger)
         {
             _shoppingList = shoppingList;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -24,18 +26,31 @@ namespace WebApplication3.Controllers
         [HttpPost]
         public void PostItem(ShoppingItemDto itemToAdd)
         {
+            _logger.LogInformation($"User is trying to add shopping list item: {itemToAdd.Name}");
             _shoppingList.AddItemToList(itemToAdd.Name);
+            _logger.LogInformation("Item was added to database");
         }
 
         [HttpPut]
         public void UpdateItem([FromQuery] int id, [FromBody] ShoppingItemDto item)
         {
-            _shoppingList.UpdateItem(id, item.Name);
+            try
+            {
+                _logger.LogInformation($"trying to update item with id {id} to be {item.Name}");
+                _shoppingList.UpdateItem(id, item.Name);
+                _logger.LogInformation("update was successful");
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"Error occured when updating item {id}");
+                throw;
+            }
         }
 
         [HttpDelete]
         public void DeleteItem(int id)
         {
+            _logger.LogWarning($"user is about to delete item with id {id}");
             _shoppingList.DeleteItem(id);
         }
     }
